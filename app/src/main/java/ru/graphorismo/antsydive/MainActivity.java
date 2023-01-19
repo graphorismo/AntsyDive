74a9646be4e3b371e689d5b28b1ecdaeb1d7356b
graphorismo <graphorismo@gmail.com>
2023-01-19 13:07:59 +0300

    Set up simple basics. 1. activity_game.xml. Created: Empty layout for the game activity. 2. activity_main.xml. Changed: simple one-button layout for the start 3. AndroidManifest.xml. Changed: Add game activity. Remove action bars from it and from the main activity. 4. build.gradle. Changed: lower required versions of dependencies to support older devices. 5. GameActivity.java. Empty game activity. 6. MainActivity.java.  Wire up the 'start' button 7. themes.xml. Chang main theme parent to support older devices.

--- app/src/main/java/ru/graphorismo/antsydive/MainActivity.java
+++ app/src/main/java/ru/graphorismo/antsydive/MainActivity.java
@@ -2,7 +2,9 @@ package ru.graphorismo.antsydive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonPlay = findViewById(R.id.mainActivity_button_play);
+        buttonPlay.setOnClickListener( (view) -> {
+            switchToGameActivity();
+        });
+    }
+
+    private void switchToGameActivity(){
+        Intent gameActivityIntent = new Intent(this, GameActivity.class);
+        startActivity(gameActivityIntent);
+        finish();
     }
 }
